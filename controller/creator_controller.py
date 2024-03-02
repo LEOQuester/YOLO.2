d7436ef0b52a906684f0019f64703017c945bbfc
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-02 07:51:12 +0530

    creator make request works

--- controller/creator_controller.py
+++ controller/creator_controller.py
@@ -11,18 +11,25 @@ CORS(creator_controller)
 class CreatorController():
     __creator_service = CreatorService()
 
+    @staticmethod
     @creator_controller.route('/', methods=['POST'])
     def update_creator():
         data = request.json
         return jsonify(
             CreatorController.__creator_service.update_to_creator(data["email"], data["phone"], data["sm_links"]))
 
+    @staticmethod
     @creator_controller.route('/', methods=['GET'])
     def get_all_creators():
         return jsonify(CreatorController.__creator_service.get_all_creators())
 
+    @staticmethod
     @creator_controller.route('/request', methods=['POST'])
     def request_application():
         data = request.json
-        return jsonify(CreatorController.__creator_service.add_request(data["email"], data['type'], data['link'],
-                                                                       data['business_email'], data['description']))
+        response = CreatorController.__creator_service.add_request(data["email"], data['type'], data['link'],
+                                                                   data['business_email'], data['description'])
+        if response["success"]:
+            return response, 200
+        else:
+            return response, 400
