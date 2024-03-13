86a5780024945737c5db0521d1aeabfc7e5c12ce
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-13 13:29:03 +0530

    added get token to service

--- controller/material_controller.py
+++ controller/material_controller.py
@@ -86,28 +86,7 @@ class MaterialController():
         return jsonify(MaterialController.__material.media_from_keywords(keywords, media_type))
 
 
-    def get_token():
-        auth_string  = CLIENT_ID + ":" + CLIENT_SECRET
-        auth_bytes = auth_string.encode('utf-8')
-        auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
 
-        url = "https://accounts.spotify.com/api/token"
-        headers = {
-            "Authorization": "Basic " + auth_base64,
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-
-        data = {"grant_type": "client_credentials"}
-        result = post(url, headers=headers, data=data)
-        json_result = json.loads(result.content)
-        token = json_result['access_token']
-        return token
-
-
-    def get_auth_header(token):
-        return {"Authorization": "Bearer " + token}
-
-    
 
     @staticmethod
     @material_controller.route('/songs', methods=[
--- service/material_service.py
+++ service/material_service.py
@@ -85,6 +85,29 @@ class Material():
                 print(f"An error occurred for {word}: {e}")
 
         return keyword_ids
+
+    def get_token():
+        auth_string  = CLIENT_ID + ":" + CLIENT_SECRET
+        auth_bytes = auth_string.encode('utf-8')
+        auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization": "Basic " + auth_base64,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        data = {"grant_type": "client_credentials"}
+        result = post(url, headers=headers, data=data)
+        json_result = json.loads(result.content)
+        token = json_result['access_token']
+        return token
+
+
+    def get_auth_header(token):
+        return {"Authorization": "Bearer " + token}
+
+    
         
     def get_songs(self, keywords, media_type):
         if media_type == 'song':
